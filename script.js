d8f43da52f5f50b89bfc71a57090425d6e756107
Inese Lapina <lapinna.inese@gmail.com>
2022-08-24 18:07:01 +0200

    function splitToDigit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*3. Write a function that takes a number (a) as argument
+Split a into its individual digits and return them in an array.*/
+
+function splitToDigit(a) {
+    return (a + "").split("").map((i) => {
+      return Number(i);
+    });
+  }
