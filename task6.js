5e10bf7c49eff0ced9ffd86d6af8f34b5312f07c
Inese Lapina <lapinna.inese@gmail.com>
2022-08-24 20:12:58 +0200

    solution for object property check

---
+++ task6.js
@@ -0,0 +1,12 @@
+function propertyExists(a, b) {
+  return a.hasOwnProperty(b);
+};
+
+/*const dog = {
+    furColor: "black",
+    name: "Aldo",
+    age: 2
+   };
+
+console.log(propertyExists(dog, "age"));
+console.log(propertyExists(dog, "height"));*/
